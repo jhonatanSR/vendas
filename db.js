286af5d7f1364b7e71bca41382c0a5d2bf1c1f3b
jhonatanSR <jhonatan_souza10@hotmail.com>
2022-08-06 03:38:24 -0300

    1.0.0.3

--- db.js
+++ db.js
@@ -6,6 +6,11 @@ async function connect(){
     global.db = await conn.db("vendas");
     return global.db;
   }
+/* USUARIOS */
+  async function findAll(){
+    const db = await connect();
+    return db.collection("usuarios").find().toArray();
+  }
 
   async function find(usuario){
     const db = await connect();
@@ -29,5 +34,55 @@ async function connect(){
     return db.collection("usuarios").updateOne(filter,{"$set":usuario});
   }
 
+  async function deleteOne(id){
+    const db = await connect(); 
+    const filter = {_id: new ObjectId(id)};
+    return db.collection("usuarios").deleteOne(filter);
+}
 
-  module.exports = {find, insert,findOne,update};
+/* PRODUTOS */
+
+  async function findAllP(){
+    const db = await connect();
+    return db.collection("produtos").find().toArray();
+  }
+
+  async function findP(produto){
+    const db = await connect();
+    return db.collection("produtos").find(produto).toArray();
+  }
+
+  async function findOneP(id){
+    const db = await connect();
+    const objId = new ObjectId(id);
+    return db.collection("produtos").findOne(objId);
+  }
+
+  async function insertP(produto){
+    const db = await connect();
+    return db.collection("produtos").insertOne(produto);
+  }
+
+  async function updateP(id,produto){
+    const filter = {_id: new ObjectId(id)}
+    const db = await connect();
+    return db.collection("produtos").updateOne(filter,{"$set":produto});
+  }
+
+  async function deleteOneP(id){
+    const db = await connect(); 
+    const filter = {_id: new ObjectId(id)};
+    return db.collection("produtos").deleteOne(filter);
+}
+
+  async function procurarP(letra){
+    const db = await connect();
+    return db.collection("produtos").find({produto: new RegExp(letra)}).sort({"produto":1}).toArray();
+  }
+
+  async function findAllPOrd(){
+    const db = await connect();
+    return db.collection("produtos").find().sort({"produto":1}).toArray();
+  }
+
+  module.exports = {findAll,find, insert,findOne,update,deleteOne,findAllP,findP,findOneP,insertP,updateP,deleteOneP,procurarP,findAllPOrd};
--- rotas/index.js
+++ rotas/index.js
@@ -21,7 +21,6 @@ async function verificaJWT(req,res,next){
 }
 
 
-
 rota.get('/', async (req,res) =>{
     res.render('index');
 })
@@ -33,20 +32,46 @@ rota.get('/cadastro', async(req,res) =>{
 rota.get('/inicio/:id',verificaJWT , async(req,res) =>{
     const id = req.params.id;
     const doc = await db.findOne(id);
-
     res.render('inicio',{doc,action: "/logout/" + id});
 })
+
+rota.get('/produtos/:id',verificaJWT,async(req,res)=>{
+    const id = req.params.id;
+    const item = req.query.item;
+    const item1 = await db.procurarP(item);
+    res.render('produtos',{action:'/cadastroP/'+id,id,item1});
+})
+
+rota.get('/deleteP/:id', async(req,res)=>{
+    const id = req.params.id;
+    const produto = req.query.produto;
+    await db.deleteOneP(produto);
+    res.redirect('/produtos/'+id);
+})
+
+rota.get('/cadastroP/:id',verificaJWT,async(req,res)=>{
+    const id = req.params.id;
+    res.render('cadastroP',{action:"/cadastroP/"+id,id,doc:{}});
+})
+
+rota.get('/editarP/:id',verificaJWT,async(req,res)=>{
+    const id = req.params.id;
+    const item = req.query.item;
+    res.render('cadastroP',{doc: await db.findOneP(item),action:"/updateP/"+id+"?item="+item,id});
+})
+
 /********************************************************/
 
 rota.post('/cadastro', async(req,res) =>{
     const dados = req.body;
-    const nome = dados.nome;
+    const nomes = dados.nome;
+    const nome = nomes.toUpperCase();
     let senha = dados.senha;
     let senhaAdm = dados.senhaAdm;
     const criadoEm = new Date().toLocaleString();
     const senhaHash = bcrypt.hashSync(senha,10);
     //const admHash = bcrypt.hashSync(senhaAdm,10);
-    const adm = await db.find({nome:"Jhonatan"});
+    const adm = await db.find({nome:"JHONATAN"});
     const user = await db.find({nome: nome});
     
     if(bcrypt.compareSync(senhaAdm, adm.senhaAdm)){
@@ -56,7 +81,7 @@ rota.post('/cadastro', async(req,res) =>{
         return res.redirect('/?sucesso');
         }
     }
-    if(senha=="" || !(bcrypt.compareSync(senhaAdm, adm.senhaAdm))){
+    if(!(bcrypt.compareSync(senhaAdm, adm.senhaAdm))){
         return res.redirect('/cadastro?fail');
     }
     return res.redirect('/cadastro?userE')
@@ -64,21 +89,20 @@ rota.post('/cadastro', async(req,res) =>{
 })
 
 rota.post('/autenticar', async(req,res,next) =>{
-    const nome = req.body.nome;
+    const nomes = req.body.nome;
+    const nome = nomes.toUpperCase();
     const senha = req.body.senha;
     const usuario = await db.find({nome});
-    const deu = bcrypt.compareSync(senha, usuario.senha);
-    const token = jwt.sign({id: usuario._id},config.secret,{expiresIn: 86400});
-    const id = usuario._id;
-    await db.update(id,{token});
-    
     if(!usuario){
         return res.redirect('/?userFail');
     }   
-
+    const deu = bcrypt.compareSync(senha, usuario.senha);
     if(!(deu)){
         return res.redirect('/?senhaFail');
     }
+    const token = jwt.sign({id: usuario._id},config.secret,{expiresIn: 86400});
+    const id = usuario._id;
+    await db.update(id,{token});
     usuario.senha = undefined;
     res.redirect('/inicio/'+usuario._id);
 
@@ -94,5 +118,38 @@ rota.post('/logout/:id', async (req,res) =>{
     res.redirect('/?logout');
     
 })
+
+rota.post('/cadastroP/:id', async(req,res)=>{
+    const id = req.params.id;
+    const produtos = req.body.produto;
+    const produto = produtos.toUpperCase();
+    const preco = req.body.preco;
+    const quantidade = req.body.qtd;
+    const codigo = req.body.cod; 
+    await db.insertP({produto,preco,quantidade,codigo});
+    res.redirect('/cadastroP/'+id);
+})
+
+rota.post('/updateP/:id', async(req,res)=>{
+    const id = req.params.id;
+    const item = req.query.item;
+    const produtos = req.body.produto;
+    const produto = produtos.toUpperCase();
+    const preco = req.body.preco;
+    const quantidade = req.body.qtd;
+    const codigo = req.body.cod;
+    await db.updateP(item,{produto,preco,quantidade,codigo});
+    res.redirect('/produtos/'+id);
+})
+
+rota.post('/findProd/:id', async(req,res)=>{
+    const id = req.params.id;
+    const produto = req.body.procurar;
+    const item = produto.toUpperCase();
+    res.redirect('/produtos/'+id+"?item="+item);
+
+
+})
+
 
 module.exports = rota;
